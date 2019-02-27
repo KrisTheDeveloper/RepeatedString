373eff229263203b124e6f86071bab450e65c084
Kris <kswhite609@hotmail.com>
2019-02-27 02:03:56 -0500

    Rev2: Contemplating using an algorithm to alleviate looping.

--- RepeatedString/RepeatedString/RepeatedString.cpp
+++ RepeatedString/RepeatedString/RepeatedString.cpp
@@ -3,9 +3,31 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+long repeatedString(string s, long n) {
+
+	string temp = "";
+
+	for (int iter = 0; iter < n; iter++)
+	{
+		temp += s;
+	}
+
+	cout << temp;
+
+	return 10000;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	float value = (10 / 3) * 2;
+	float value2 = static_cast<float>(10) / 3;
+	repeatedString("aba", 7);
+	return 0;
 }
 
