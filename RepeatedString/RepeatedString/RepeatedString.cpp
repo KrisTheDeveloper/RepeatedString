dd240c5c89e56c0aa8da193fc3d2b862f2c32062
Kris <kswhite609@hotmail.com>
2019-02-27 17:23:40 -0500

    Rev5: Modified the algorithm to split between a complete string repeat and fractional string repeat.
    After whole duplicate is captured. calculate percentage of whole to get remainder and loop to capture left over characters.

--- RepeatedString/RepeatedString/RepeatedString.cpp
+++ RepeatedString/RepeatedString/RepeatedString.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <cstring>
 #include <vector>
+#include <math.h>
 
 
 using namespace std;
@@ -17,20 +18,33 @@ long long repeatedString(string s, long long n) {
 	if (count == s.size())
 		return n;
 
+	const char *p = s.c_str();
 
-	long double totalACount = 0;
-	totalACount = (static_cast<long double>(n) / s.size()) * count;
-
-	if (static_cast<long long>(totalACount * 10 ) % 10> 0)
-		totalACount += 1;
+	long double base = (static_cast<long double>(n) / s.size());
+	long long totalACount = static_cast<long long>(base) * count;
+	
+	long double checkLeftOver = ((static_cast<long long>(base * 100) % 100) /100.0) * s.size();
+	
+	for (int iter = 0; iter < checkLeftOver; iter++)
+	{
+		if (p[iter] == 'a')
+			totalACount += 1;
+	}	
 	
 	return totalACount;
 }
 
 int main()
 {
-	int answer = repeatedString("aba", 10);
+	/* expected: 16481469408*/
+	//long long answer = repeatedString("epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq", 160);//549382313570);
+	//long long answer = repeatedString("epsvwctmekpsnamchznsoxak", 45);
+	//long answer = repeatedString("gfcaaaecbg", 547602);
+	//int answer = repeatedString("aba", 10);
+	//long long answer = repeatedString("aab", 7);
 	//long long answer = repeatedString("kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm", 736778906400);
+	//long long answer = repeatedString("babbaabbabaababaaabbbbbbbababbbabbbababaabbbbaaaaabbaababaaabaabbabababaabaabbbababaabbabbbababbaabb", 860622337747);
+	long long answer = repeatedString("bcbccacaacbbacabcabccacbccbababbbbabcccbbcbcaccababccbcbcaabbbaabbcaabbbbbbabcbcbbcaccbccaabacbbacbc", 649606239668);
 	return 0;
 }
 
